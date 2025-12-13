9927748a3606802eab4ccde4029f837ca48eef5b
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2025-12-13 19:37:37 +0100

    fix projectile clean-up

--- projectile.gd
+++ projectile.gd
@@ -23,3 +23,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_lifetime_timeout() -> void:
 	pass
 	#queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
