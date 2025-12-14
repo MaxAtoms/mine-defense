0f180261ed55b385052ad79d66cdf58d65892416
Oliver Gerstl <oliver.gerstl@uni-ulm.de>
2025-12-14 14:30:21 +0100

    Allow goblins to attack targets

---
+++ entity/enemy/attacker.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var damage = 10
+@export var interval = 2
+@export var target_groups = ["target", "defence"]
+
+var target: Node2D = null
+
+func _ready() -> void:
+	$AttackTimer.wait_time = interval
+
+func _on_body_entered(body: Node2D) -> void:
+	if target_groups.any(func (group): return body.is_in_group(group)):
+		target = body
+		if $AttackTimer.is_stopped():
+			print("Damaging: ", target.name)
+			target.get_node("Damagable").damage(damage)
+			$AttackTimer.start()
+
+func _on_body_exited(body: Node2D) -> void:
+	if target == body:
+		target = null
+		$AttackTimer.stop()
+
+
+func _on_attack_timer_timeout() -> void:
+	if is_instance_valid(target):
+		print("Damaging ", target.name)
+		target.get_node("Damagable").damage(damage)
+	else:
+		target = null
