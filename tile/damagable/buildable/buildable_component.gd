e54c91b3f2d45fa81c016b0b42a70afecb00c602
Henry Schuler <henryschuler@outlook.de>
2025-12-14 17:52:29 +0100

    Fixed building

--- tile/damagable/buildable/buildable_component.gd
+++ tile/damagable/buildable/buildable_component.gd
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 	pass
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("build"):
+	if event.is_action_pressed("build") and event.device == get_parent().device_id:
 		var bag_type = get_parent().get_bag_type()
 
 		var instance
