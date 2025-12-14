94e723319c1940d30c43dc67fa7aa629cbae29f0
Oliver Gerstl <oliver.gerstl@uni-ulm.de>
2025-12-14 16:35:12 +0100

    Fix crfating structure collision

--- tile/damagable/target/worker/stonemason.gd
+++ tile/damagable/target/worker/stonemason.gd
@@ -1,5 +1,7 @@
 extends Worker
 
+@onready var main = get_tree().get_root().get_node("Map")
+@onready var rubble_scene = preload("res://tile/damagable/buildable/defence/Rubble.tscn")
 
 func _init():
 	worker_name = "stonemason"
@@ -10,3 +12,9 @@ func _init():
 	produced_items_per_request = 2
 	cooldown_in_sec = 1
 	interacting_component = null
+
+func _on_damagable_on_death() -> void:
+	queue_free()
+	var rubble = rubble_scene.instantiate()
+	rubble.global_position = global_position
+	main.add_child(rubble)
